451302663edbf01f9aa87570bedb2d29d0ad7348
Stefan Rado <github@kroimon.de>
2023-11-08 21:40:20 +0100

    docs: switch docs theme to furo

--- docs/conf.py
+++ docs/conf.py
@@ -3,17 +3,13 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
+# Project information
 project = "SkyRC BLE"
 copyright = "2023, Stefan Rado"
 author = "Stefan Rado"
 release = "2.0.1"
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
+# General configuration
 extensions = [
     "myst_parser",
 ]
@@ -23,13 +19,15 @@ source_suffix = [
     ".rst",
     ".md",
 ]
+templates_path = [
+    "_templates",
+]
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+]
 
-templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = "sphinx_rtd_theme"
+# Options for HTML output
+html_theme = "furo"
 html_static_path = ["_static"]
